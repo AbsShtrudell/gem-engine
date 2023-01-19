cb58a5fddc7443ca7a4259c0951b07a8ff86bb9a
Shtrudell <70441070+AbsShtrudell@users.noreply.github.com>
2023-01-19 16:07:27 +0300

    tests setup

---
+++ tests/src/tests.cpp
@@ -0,0 +1,15 @@
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace tests
+{
+	TEST_CLASS(tests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
